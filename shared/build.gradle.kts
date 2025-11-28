b7a2b21aeb8530c990210c2c336f5f6438b044f8
Ricardo Martins <rikhard.martins@gmail.com>
2025-11-28 14:40:22 +0000

    Added Kermit and Ktor client logging to the project.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -40,6 +40,8 @@ kotlin {
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.client.resources)
             implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.ktor.client.logging)
+            implementation(libs.kermit)
             implementation(libs.koin.core)
             implementation(libs.kotlinx.datetime)
             api(libs.kmp.observable.viewmodel)
