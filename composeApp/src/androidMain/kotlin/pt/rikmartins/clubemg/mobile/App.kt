575d78a81d9719ddcc0238528325de0fa0358d62
Ricardo Martins <rikhard.martins@gmail.com>
2025-12-10 12:01:36 +0000

    Added an App specific theme.

--- composeApp/src/androidMain/kotlin/pt/rikmartins/clubemg/mobile/App.kt
+++ composeApp/src/androidMain/kotlin/pt/rikmartins/clubemg/mobile/App.kt
@@ -1,10 +1,6 @@
 package pt.rikmartins.clubemg.mobile
 
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -13,6 +9,7 @@ import androidx.navigation.toRoute
 import kotlinx.serialization.Serializable
 import pt.rikmartins.clubemg.mobile.ui.EventDetailScreen
 import pt.rikmartins.clubemg.mobile.ui.CalendarScreen
+import pt.rikmartins.clubemg.mobile.ui.theme.AppTheme
 
 @Serializable
 object CalendarDestination
@@ -22,9 +19,7 @@ data class EventDetailDestination(val objectId: Int)
 
 @Composable
 fun App() {
-    MaterialTheme(
-        colorScheme = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme()
-    ) {
+    AppTheme {
         Surface {
             val navController = rememberNavController()
             NavHost(navController = navController, startDestination = CalendarDestination) {
