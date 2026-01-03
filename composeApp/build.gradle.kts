e6e7af862c35b69d0caa006df6557f94374b48c2
Ricardo Martins <rikhard.martins@gmail.com>
2026-01-03 14:14:57 +0000

    Corrected the version name.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -51,7 +51,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1"
     }
     packaging {
         resources {
