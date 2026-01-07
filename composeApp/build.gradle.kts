6405a647a6d4945dc645a569801f1f76cfee3829
Ricardo Martins <rikhard.martins@gmail.com>
2026-01-07 15:12:03 +0000

    Version bump.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "pt.rikmartins.clubemg.mobile"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.1.0"
+        versionCode = 4
+        versionName = "0.1.1"
     }
     packaging {
         resources {
