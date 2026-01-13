e8752e86e6c074d9fa11fef475398affc660129c
Ricardo Martins <rikhard.martins@gmail.com>
2026-01-13 20:11:27 +0000

    Added logging to SynchronizeFavouriteEvents.kt

--- shared/src/commonMain/kotlin/pt/rikmartins/clubemg/mobile/domain/usecase/events/SynchronizeFavouriteEvents.kt
+++ shared/src/commonMain/kotlin/pt/rikmartins/clubemg/mobile/domain/usecase/events/SynchronizeFavouriteEvents.kt
@@ -1,17 +1,25 @@
 package pt.rikmartins.clubemg.mobile.domain.usecase.events
 
+import co.touchlab.kermit.Logger
 import pt.rikmartins.clubemg.mobile.domain.usecase.base.UseCase
 
 class SynchronizeFavouriteEvents(
     private val favouriteProvider: FavouriteProvider,
     private val eventsProvider: EventsProvider,
     private val notifier: Notifier,
+    private val logger: Logger = Logger.withTag(SynchronizeFavouriteEvents::class.simpleName!!)
 ) : UseCase<Unit, Unit>() {
 
     override suspend fun execute(params: Unit) {
+        logger.v { "Synchronizing favourite events" }
         val favouriteEventsIds = favouriteProvider.getAllFavouriteEventsIds()
+        logger.d { "Favourite events are: $favouriteEventsIds" }
         val eventDiffs = eventsProvider.refreshEventsForDiff(favouriteEventsIds)
-        if (eventDiffs.isNotEmpty()) notifier.notifyFavouriteEventsChanged(eventDiffs)
+        logger.d { "Detected events changes: $eventDiffs" }
+        if (eventDiffs.isNotEmpty()) {
+            logger.v { "Notifying favourite events changes" }
+            notifier.notifyFavouriteEventsChanged(eventDiffs)
+        } else logger.v { "No notification of favourite events changes" }
     }
 
     interface FavouriteProvider {
